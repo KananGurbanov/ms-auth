cdd058e501129a231ab2c2922294f7a15e35c47f
TheCoteen <knanqurbanov22@gmail.com>
2024-10-24 16:25:50 +0400

    login and logout logic added

--- src/main/java/az/edu/turing/config/SecurityConfig.java
+++ src/main/java/az/edu/turing/config/SecurityConfig.java
@@ -26,7 +26,6 @@ import static org.springframework.security.config.http.SessionCreationPolicy.STA
 public class SecurityConfig {
 
     private static final String[] WHITE_LIST_URL = {
-            "/api/v1/rates",
             "/api/v1/auth/**",
             "/v2/api-docs",
             "/v3/api-docs",
