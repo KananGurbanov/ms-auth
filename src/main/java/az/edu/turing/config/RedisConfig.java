a1d8710c6ee95efed40990e818f7c20a4948e82d
Kanan Gurbanov <knanqurbanov22@gmail.com>
2024-11-21 17:36:21 +0400

    redis config changed

--- src/main/java/az/edu/turing/config/RedisConfig.java
+++ src/main/java/az/edu/turing/config/RedisConfig.java
@@ -18,15 +18,26 @@ public class RedisConfig {
     @Value("${spring.data.redis.port}")
     private Integer redisPort;
 
+    @Value("${spring.data.redis.password:}")
+    private String redisPassword;
+
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory(redisHost, redisPort);
+        LettuceConnectionFactory connectionFactory = new LettuceConnectionFactory(redisHost, redisPort);
+        if (!redisPassword.isEmpty()) connectionFactory.setPassword(redisPassword);
+        return connectionFactory;
     }
 
     @Bean
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(connectionFactory);
+
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setHashKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        template.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
+
         return template;
     }
 }
